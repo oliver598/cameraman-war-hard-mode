d33e54844922aa891617384dec50c567adf06c41
oliver598 <oliverdibdob@outlook.com>
2025-02-01 17:27:07 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "scibidi":return img`
+f f f f f . . . . . . . . . . . 
+f f f f f . . . . . . . . . . . 
+f f f f f d d d d d d . . . . . 
+f f f f f d f d d f d . . . . . 
+f f f f f d d d d d d . . . . . 
+f f f f f d f d d f d . . . . . 
+f f f f f d d f f d d . . . . . 
+f f f f f . . d d . . . . . . . 
+f f f f f . . d d . . . . . . . 
+f f f f f . . d d . . . . . . . 
+f f f f f . . d d . . . . . . . 
+f f f f f f f f f f f f . . . . 
+f f f f f f f f f f f f . . . . 
+f f f f f f f f f f f f . . . . 
+f f f f f f f f f f f f . . . . 
+f f f f f f f f f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
